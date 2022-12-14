469e6eb449d0139529387f5fa024a3285922a6ad
diogo-h <diogo.henriques@ontopstudios.com>
2022-12-14 13:46:23 +0000

    Add Joi Valdiations

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const {campgroundSchema} = require("./schemas.js");
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require("./models/campground");
+const { join } = require("path");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -24,6 +26,17 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"));
 
+const validateCampground = (req, res, next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(",")
+        throw new ExpressError(msg, 400)
+    } 
+    else{
+        next();
+    }
+}
+
 app.get("/", (req, res) => {
     res.render("home")
 })
@@ -37,8 +50,8 @@ app.get('/campgrounds/new', (req, res) => {
     res.render("campgrounds/new")
 })
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    //if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -60,7 +73,7 @@ app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+app.delete('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndDelete(id)
     res.redirect("/campgrounds")
